4f3143b32b5f63328a9efe09b9c6d59d43f0ad38
Henry Caldwell <henrycaldwell2005@gmail.com>
2026-06-27 17:26:45 -0400

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-zinc-900 text-white flex items-center justify-center text-2xl font-bold">
+      Streamline Console
+    </div>
+  );
+}
+
+export default App;
